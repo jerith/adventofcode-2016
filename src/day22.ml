7f90cf584b781576a8392c6dae7ebc8a9f51b132
Jeremy Thurgood <firxen@gmail.com>
2016-12-23 16:46:29 +0200

    The rest of day 22.

--- src/day22.ml
+++ src/day22.ml
@@ -18,7 +18,6 @@ module Parser = struct
   let stats = lift4 tuple4 tb tb tb perc
   let node = lift2 tuple2 device stats
 
-
   let nodes = many1 header *> sep_by end_of_line node
 end
 
@@ -27,24 +26,26 @@ type storage = {
   nodes : (int * int) PairsMap.t;
   height : int;
   width : int;
+  walls : PairsMap.key list;
 }
 
 let get st n = (n, PairsMap.find n st.nodes)
-let pos (n, _) = n
 
 let node_to_str ((x, y), (s, u, a, p)) =
   Printf.sprintf
     "/dev/grid/node-x%d-y%d\t%4dT S\t%4dT U\t%4dT A\t(%d%%)" x y s u a p
 
 let setup_nodes nodelist =
-  let rec add' nodes = function
-    | [] -> nodes
-    | (n, (_, u, a, _)) :: nodelist ->
-      add' (PairsMap.add n (u, a) nodes) nodelist
+  let rec add' nodes walls = function
+    | [] -> nodes, walls
+    | (n, (c, u, a, _)) :: nodelist ->
+      let nodes = PairsMap.add n (u, a) nodes in
+      let walls = if c > 100 then n :: walls else walls in
+      add' nodes walls nodelist
   in
-  let nodes = add' PairsMap.empty nodelist in
+  let nodes, walls = add' PairsMap.empty [] nodelist in
   let (mx, my), _ = PairsMap.max_binding nodes in
-  {nodes; height = (my + 1); width = (mx + 1)}
+  {nodes; height = (my + 1); width = (mx + 1); walls}
 
 let can_move (n0, (u0, _)) (n1, (_, a1)) = n0 <> n1 && u0 <> 0 && u0 <= a1
 
@@ -58,8 +59,8 @@ let viable_pairs st =
   find' [] (PairsMap.bindings st.nodes)
 
 let adj_nodes st (x, y) =
-  [x-1, y; x+1, y; x, y-1; x, y+1] |>
-  List.filter (fun (x, y) -> PairsMap.mem (x, y) st.nodes) |>
+  [x, y-1; x+1, y; x-1, y; x, y+1] |>
+  List.filter (fun n -> PairsMap.mem n st.nodes) |>
   List.map (get st)
 
 let move_data st (n0, (u0, a0)) (n1, (u1, a1)) =
@@ -69,23 +70,18 @@ let move_data st (n0, (u0, a0)) (n1, (u1, a1)) =
 let viable st n = List.exists (can_move (get st n)) (adj_nodes st n)
 let target st n = List.exists (fun a -> can_move a (get st n)) (adj_nodes st n)
 
-let print_nodes st =
+let print_nodes st data =
   let row y = Array.init st.width id |> Array.map (fun x -> x, y) in
-  (* let mapnode f n = let u, a = PairsMap.find n nodes in f u a in *)
-  (* let n2str u a = Printf.sprintf "%3d:%-2d" u a in *)
-  (* let n2cstr u a = Printf.sprintf "  %3d " (u+a) in *)
   let n2vstr n =
-    match target st n, viable st n with
-    | true, true -> " @" | true, false -> " #"
-    | false, true -> " o" | false, false -> " ."
+    match n = data, List.mem n st.walls with
+    | true, _ -> "d"
+    | false, true -> "w"
+    | _ ->
+      match target st n, viable st n with
+      | true, true -> "@" | true, false -> "#"
+      | false, true -> "o" | false, false -> "."
   in
-  let row2str y =
-    String.concat "\n" [
-      (* map_to_str " " (mapnode n2str) (row y |> Array.to_list); *)
-      (* map_to_str " " (mapnode n2cstr) (row y |> Array.to_list); *)
-      map_to_str " " n2vstr (row y |> Array.to_list);
-    ]
-  in
+  let row2str y = map_to_str " " n2vstr (row y |> Array.to_list) in
   map_to_str "\n" row2str (Array.init st.height id |> Array.to_list) |>
   noise_endline
 
@@ -101,52 +97,78 @@ let get_targets st =
   let add_target n d ts = if target st n then (n :: ts) else ts in
   PairsMap.fold add_target st.nodes []
 
-let get_viable st t =
-  adj_nodes st t |> List.filter (fun n -> can_move n (get st t))
+let get_viable st target =
+  adj_nodes st target |> List.filter (fun n -> can_move n (get st target))
+
+let wall_penalty st (tx, ty) =
+  let rec checkwall' penalty = function
+    | [] -> penalty
+    | (wx, wy) :: _ when tx = wx && ty >= wy -> 200 + wx*2
+    | (_, wy) :: walls when ty >= wy -> checkwall' (max penalty 100) walls
+    | _ :: walls -> checkwall' penalty walls
+  in checkwall' 0 st.walls
 
 let calc_score st steps (dx, dy) (tx, ty) =
-  List.length steps + dx + dy + abs (dx - tx) + abs (dy - ty)
+  List.fold_left (+) 0 [
+    List.length steps;
+    (dx + dy)*10;
+    wall_penalty st (tx, ty);
+    (abs (dx - tx) + abs (dy - ty))*2]
 
 let make_move_for move target n =
   let st = move_data move.st n (get move.st target) in
-  let steps = (target, pos n) :: move.steps in
-  let score = calc_score st steps move.data target in
-  {move with st; steps; target; score}
-
-let make_moves_to move t =
-  let viables = get_viable move.st t in
-  List.map (make_move_for move t) viables
+  let steps = (target, fst n) :: move.steps in
+  let data = if move.data = fst n then target else move.data in
+  let target = fst n in
+  let score = calc_score st steps data target in
+  {st; steps; data; target; score}
 
 let step_to_str ((x0, y0), (x1, y1)) =
   Printf.sprintf "%d,%d->%d,%d" x0 y0 x1 y1
 
-let print_steps steps = map_to_str " " step_to_str steps |> noise_endline
+let print_move move () =
+  noisef "Score: %d Steps: %d, t = %d,%d\n"
+    move.score (List.length move.steps) (fst move.target) (snd move.target);
+  print_nodes move.st move.data
 
-let rec make_move = function
-  (* | [] -> failwith "Failed. :-(" *)
-  | ml when List.length ml > 100 -> failwith "Too many."
-  | [] -> 0
-  | move :: moves ->
-    noisef "Steps: %d, target= %d,%d\n" (List.length move.steps) (fst move.target) (snd move.target);
-    print_steps move.steps;
-    print_nodes move.st;
-    if move.target = (0, 0) then failwith "foo" else ();
-    let targets = get_targets move.st in
-    let movelists = List.map (make_moves_to move) targets in
-    let moves =
-      List.concat (moves :: movelists) |>
-      List.sort (fun a b -> compare a.score b.score)
+
+module Moves = struct
+  type t = move
+  let comptuple m = m.score, List.length m.steps, m.data, m.target
+  let compare a b = compare (comptuple a) (comptuple b)
+end
+module MovesSet = Set.Make(Moves)
+
+let pop_move moves =
+  let move = MovesSet.min_elt moves in
+  MovesSet.remove move moves, move
+
+let add_move moves move = MovesSet.add move moves
+
+let rec make_move moves =
+  match pop_move moves with
+  | moves, move when move.data = (0, 0) ->
+    noisef "Done! "; ifnoise (print_move move);
+    List.length move.steps
+  | moves, move ->
+    ifnoise (print_move move);
+    let newmoves =
+      get_viable move.st move.target |>
+      List.map (make_move_for move move.target)
     in
-    make_move moves
+    make_move @@ List.fold_left add_move moves newmoves
 
-let first_move st = {
+let first_move st target = {
   st;
   steps = [];
-  data = (0, st.width - 1);
-  target = (0, st.width - 1);
+  data = (st.width - 1, 0);
+  target = target;
   score = 0;
 }
 
+let first_moves st =
+  get_targets st |> List.map (first_move st) |>
+  List.fold_left add_move MovesSet.empty
 
 let main_1 input =
   noise_endline @@ map_to_str "\n" node_to_str input;
@@ -157,8 +179,8 @@ let main_1 input =
 
 let main_2 input =
   let st = setup_nodes input in
-  print_nodes st;
-  make_move [(first_move st)] |> string_of_int
+  let moves = first_moves st in
+  make_move moves |> string_of_int
 
 
 type t = node list
